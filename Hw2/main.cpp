cee7cad4cc6e5fe49705cd6cd4737180f7417f06
gsapoz <garysapozhn@gmail.com>
2014-09-21 16:35:21 -0400

    updated main

--- Hw2/main.cpp
+++ Hw2/main.cpp
@@ -15,20 +15,20 @@ Entity ball;  //pong ball
 
 GLuint LoadTexture(const char* image_path, GLenum image_format = GL_BGRA) {
     //Function Acquired in Lecture Notes
-    SDL_Surface *surface = IMG_Load(image_path);
+	SDL_Surface *surface = IMG_Load(image_path);
     
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
     
-    glTexImage2D(GL_TEXTURE_2D, 0, 4, surface->w, surface->h, 0, image_format, GL_UNSIGNED_BYTE, surface->pixels);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, surface->w, surface->h, 0, image_format, GL_UNSIGNED_BYTE, surface->pixels);
     
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
     SDL_FreeSurface(surface);
     
-    return textureID;
+	return textureID;
 }
 
 void Setup() {
@@ -36,6 +36,7 @@ void Setup() {
 	SDL_Init(SDL_INIT_VIDEO);
 	displayWindow = SDL_CreateWindow("Pong!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+
 	SDL_GL_MakeCurrent(displayWindow, context);
     
 	glViewport(0, 0, 800, 600);
@@ -47,11 +48,11 @@ bool ProcessEvents() {
 	//Function Acquired in Lecture Notes
 	SDL_Event event;
     
-    while (SDL_PollEvent(&event)){
-        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-            return false;
-        }
-    }
+	while (SDL_PollEvent(&event)){
+		if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+			return false;
+		}
+	}
 
 	//key-input (A/Z controls player 1 paddle, Up/Down controls AI paddle.  
 	const Uint8* cmd = SDL_GetKeyboardState(NULL);
@@ -101,7 +102,7 @@ bool ProcessEvents() {
 		ball.dir_y = aiPaddle.dir_y * aiPaddle.speed;
 	} 
     
-    return true;
+	return true;
 }
 
 void Update(float elapsed) {
@@ -143,9 +144,9 @@ void Render() {
 //void Cleanup();
 int main(int argc, char *argv[])
 {
-    Setup();
+	Setup();
     
-    float lastFrameTicks = 0.0f;
+	float lastFrameTicks = 0.0f;
     
     //ball/paddle.textureID / x / y ...
 	ball.textureID = LoadTexture("ballBlu.png");
@@ -161,13 +162,12 @@ int main(int argc, char *argv[])
 
 	//ProcessEvents();
 	while (ProcessEvents()) {
-        float ticks = (float)SDL_GetTicks() / 1000.0f;
-        float elapsed = ticks - lastFrameTicks;
-        lastFrameTicks = ticks;
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+		Render(); 
+	}
 
-        Render(); 
-    }
-    
 	SDL_Quit();
 	return 0;
 }
